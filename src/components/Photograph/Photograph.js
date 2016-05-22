bfb50baf7d780c6a8d943874fa52df5eaa7c7f0e
David Gomes <davidrafagomes@gmail.com>
2016-05-22 15:27:06 +0100

    implements image thumbnail vs actual url

--- src/components/Photograph/Photograph.js
+++ src/components/Photograph/Photograph.js
@@ -4,12 +4,14 @@ import classes from './Photograph.scss'
 export default class Photograph extends React.Component {
   render() {
     return (
-      <img
-        alt='alt text'
-        className={classes.photograph}
-        width="300"
-        height="300"
-        src={this.props.url} />
+      <a target="_blank" href={this.props.url}>
+        <img
+          alt='alt text'
+          className={classes.photograph}
+          width="300"
+          height="300"
+          src={this.props.thumbnail} />
+      </a>
     );
   }
 }
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -49,7 +49,7 @@ export default class HomeView extends React.Component {
 
         <ul>
           {this.state.images.map((image, index) =>
-            <Photograph url={image.url} tags={image.tags} />
+            <Photograph url={image.url} thumbnail={image.thumbnail} tags={image.tags} />
           )}
         </ul>
       </div>
