4f872ef581e1a701b19e33b26bc67f2d5f81d592
David Gomes <davidrafagomes@gmail.com>
2016-05-22 17:00:53 +0100

    more photographs and puts footer in new position

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -59,6 +59,25 @@ export default class HomeView extends React.Component {
             <Photograph key={index} url={image.url} thumbnail={image.thumbnail} tags={image.tags} />
           )}
         </ul>
+
+        <p>
+        <a rel="license"
+           href="http://creativecommons.org/publicdomain/zero/1.0/">
+          <img src="https://licensebuttons.net/p/zero/1.0/88x31.png" style={{borderStyle: "none"}} alt="CC0" />
+        </a>
+        <br />
+        To the extent possible under law,
+        <a rel="dct:publisher"
+           href="improvecoimbra.org">
+          <span property="dct:title">Improve Coimbra</span></a>
+        has waived all copyright and related or neighboring rights to 
+        <span property="dct:title">Coimbra Photography</span>.
+      This work is published from:
+      <span property="vcard:Country" datatype="dct:ISO3166"
+            content="PT" about="improvecoimbra.org">
+        Portugal</span>.
+      </p>
+
       </div>
     );
   }
