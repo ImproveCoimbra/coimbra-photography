0c7b3c8743c5f1cdf2165f5ac86742f7d14ca3b4
David Gomes <davidrafagomes@gmail.com>
2016-05-22 15:58:58 +0100

    creates intro description paragraph at the top

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -42,6 +42,13 @@ export default class HomeView extends React.Component {
   render() {
     return (
       <div>
+        <p id={classes.introDescription}>
+          O "Coimbra Photography" é um repositório online de fotografias de Coimbra que tem
+          como objetivo ajudar qualquer pessoa que precise de uma fotografia da cidade. Estas
+          fotografias estão licenseadas sob a licença CC0, que permite que qualquer pessoa
+          utilize ou redistribue as mesmas. Este website foi criado no Improve Coimbra.
+        </p>
+
         <input id={classes.searchBar}
                placeholder="Pesquisa (baixa, jardim)"
                onChange={this.handleSearch.bind(this)}
