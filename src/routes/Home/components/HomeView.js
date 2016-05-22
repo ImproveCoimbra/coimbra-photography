879e21721d681c15dc00c44a54d57276bfede09f
David Gomes <davidrafagomes@gmail.com>
2016-05-22 16:20:47 +0100

    adds description text and CC0 footer

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -45,7 +45,7 @@ export default class HomeView extends React.Component {
         <p id={classes.introDescription}>
           O "Coimbra Photography" é um repositório online de fotografias de Coimbra que tem
           como objetivo ajudar qualquer pessoa que precise de uma fotografia da cidade. Estas
-          fotografias estão licenseadas sob a licença CC0, que permite que qualquer pessoa
+          fotografias estão disponíveis sob a licença CC0, que permite que qualquer pessoa
           utilize ou redistribue as mesmas. Este website foi criado no Improve Coimbra.
         </p>
 
